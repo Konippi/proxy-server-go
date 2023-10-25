d21b34ff483fb31a4cac98e12e6ab87eb7a13e0a
Konippi <konippi-114@outlook.jp>
2023-10-25 23:56:03 +0900

    add: date

--- run.sh
+++ run.sh
@@ -1,17 +1,19 @@
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
+
 case "$1" in
 "")
     # Run docker container
-    echo "[run.sh] Docker conatiner is running..."
+    echo "(run.sh) [$DATE] Docker conatiner is running..."
     docker compose up -d
 
     # Run server
-    echo "[run.sh] Server is running..."
+    echo "(run.sh) [$DATE] Server is running..."
     cd src
     go run main.go
     ;;
 "stop")
     # Stop docker container
-    echo "[run.sh] Docker conatiner is stopping..."
+    echo "(run.sh) [$DATE] Docker conatiner is stopping..."
     docker compose down
     ;;
 esac
