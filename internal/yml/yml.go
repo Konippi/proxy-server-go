c26ba7c8e8cc8b0c73919c74b6e5a27c8ed8a4a3
Konippi <konippi-114@outlook.jp>
2024-01-13 16:39:11 +0900

    fix: name of vars

--- internal/yml/yml.go
+++ internal/yml/yml.go
@@ -7,14 +7,14 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-func Deserialize[T any](path string, tStruct *T) error {
+func Deserialize[T any](path string, target *T) error {
 	f, err := os.Open(path)
 	if err != nil {
 		return errors.Wrap(err, "Failed to open yml file")
 	}
 	defer f.Close()
 
-	err = yaml.NewDecoder(f).Decode(&tStruct)
+	err = yaml.NewDecoder(f).Decode(&target)
 	if err != nil {
 		return errors.Wrap(err, "Failed to decode yml file")
 	}
